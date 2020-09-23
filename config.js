b799a93d161d16bc8c330a5921e0a467d3d5beb0
春田石雨 <flowke@163.com>
2020-09-23 17:45:25 +0800

    u

--- config.js
+++ config.js
@@ -5,21 +5,21 @@ module.exports = {
   USER_HOME: process.env.HOME || process.env.USERPROFILE,
   // https: //developer.mozilla.org/zh-CN/docs/Web/HTTP/CSP
   removeCSP: true,
-  redirect: (url, r) => {
-    // console.log(url);
-    // if (url.indexOf('activity.xueersi.com/lightliveclasstable') !== -1) {
+  // redirect: (url, r) => {
+  //   // console.log(url);
+  //   // if (url.indexOf('activity.xueersi.com/lightliveclasstable') !== -1) {
 
-    //   return 'http://192.168.50.104:8080/'
-    // }
+  //   //   return 'http://192.168.50.104:8080/'
+  //   // }
     
-  },
-  injectScripts: [
-    '//abc.com/cc.js',
-    {
-      target: 'xueersi.com',
-      path: 'c.com'
-    }
-  ],
+  // },
+  // injectScripts: [
+  //   '//abc.com/cc.js',
+  //   {
+  //     target: 'xueersi.com',
+  //     path: 'c.com'
+  //   }
+  // ],
   anyProxy:{
     silent: true
   }
